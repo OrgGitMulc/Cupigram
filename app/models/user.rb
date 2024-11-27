f9ee061124c512af0dca705585dd426f75e7f90f
GitMulC <cmulvo@gmail.com>
2024-11-27 23:58:02 +0000

    Add navbar & logic for duplicate email on sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :recipe_histories, dependent: :destroy
+  # Ensure email is unique
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  
+  # For consistent storage
+  before_save :normalize_email
+
+  private
+
+  # Converts email to lower case
+  def normalize_email
+    self.email = email.downcase.strip
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_17_212553) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_27_231510) do
   create_table "recipe_histories", force: :cascade do |t|
     t.integer "user_id", null: false
     t.json "ingredients", default: []
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_17_212553) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "recipe_histories", "users"
