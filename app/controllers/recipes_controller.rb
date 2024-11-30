70e3f000373a66ceb35f4a86276c1d88782bf283
GitMulC <cmulvo@gmail.com>
2024-11-30 17:29:06 +0000

    Create error if no URL is entered for extract method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,11 @@ class RecipesController < ApplicationController
   end
 
   def extract
+    if params[:url].blank?
+      flash[:alert] = "URL is required to extract the recipe."
+      return redirect_to recipes_path  # Or render :index if you prefer not to redirect
+    end
+
     url = params[:url]
     headers = { "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36" }
     unit = params[:unit]
