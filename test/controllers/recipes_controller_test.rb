726cbcc4dc53c1745e892867c40d68c3d3310224
GitMulC <cmulvo@gmail.com>
2024-12-03 22:30:28 +0000

    Add base case test for extract method in recipes controller

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,8 +1,16 @@
 require "test_helper"
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get recipes_index_url
+  setup do
+    User.delete_all
+    RecipeHistory.delete_all
+
+    @user = User.create!(id: 1, email: "user1@example.com", password_digest: "password")
+    @recipe_history = RecipeHistory.create!(id: 1, url: "https://example.com/recipe1", user_id: @user.id, ingredients: [])
+  end
+
+  test "extract & convert recipe with valid URL to desired unit" do
+    post extract_recipes_url, params: { url: "https://www.allrecipes.com/mini-chicken-pot-pies-recipe-8735456", unit: "metric" }
     assert_response :success
   end
 end
