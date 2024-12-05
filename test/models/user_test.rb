e2a891679209f71667a904518096c82eb068dd65
GitMulC <cmulvo@gmail.com>
2024-12-05 23:40:50 +0000

    Add tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
+# Test Type: Model Tests
+# Create and validate unique email.
+# Ensure password is encrypted and saved correctly.
+
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    User.delete_all
+    RecipeHistory.delete_all
+  end
+
+  test "should validate unique email" do
+    user = User.create!(id: 1, email: "user1@example.com", password: "password")
+    duplicate_user = User.create(email: user.email, password: user.password)
+    assert_not duplicate_user.valid?
+    assert_includes duplicate_user.errors[:email], "has already been taken"
+  end
+
+  test "should create encrypted password" do
+    user = User.create(email: "user2@example.com", password: "password123")
+    assert user.authenticate("password123")
+  end
 end
