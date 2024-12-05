8b7f5e327864ba83b0c45f77cbcab3a9107ddb6a
GitMulC <cmulvo@gmail.com>
2024-12-05 23:39:17 +0000

    Add model testing for user model

--- test/models/recipe_history_test.rb
+++ test/models/recipe_history_test.rb
@@ -1,7 +1,18 @@
+# Test Type: Model Tests
+# Validate saving and retrieving recipe histories.
+
 require "test_helper"
 
 class RecipeHistoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    User.delete_all
+    RecipeHistory.delete_all
+  end
+
+  test "should save & retrieve a recipe history from a given user" do
+    user = User.create(id: 1, email: "user1@example.com", password: "password123")
+    recipe_history = RecipeHistory.create!(id: 1, url: "https://validrecipe.com", user_id: user.id, ingredients: ["1g flour"])
+    assert_equal "https://validrecipe.com", recipe_history.url
+    assert_includes recipe_history.ingredients, "1g flour"
+  end
 end
