d0950d3e2735552a06074eca5274d0c0f2fd30c7
GitMulC <cmulvo@gmail.com>
2024-12-05 23:38:28 +0000

    Add additional tests for recipes controller

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,3 +1,8 @@
+# Test Type: Controller Tests
+# Ensure the extract method handles valid inputs correctly.
+# Test edge cases, such as missing or invalid URLs.
+# Verify that recipe histories are saved to the database when logged in.
+
 require "test_helper"
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
@@ -5,12 +10,23 @@ class RecipesControllerTest < ActionDispatch::IntegrationTest
     User.delete_all
     RecipeHistory.delete_all
 
-    @user = User.create!(id: 1, email: "user1@example.com", password_digest: "password")
+    @user = User.create!(id: 1, email: "user1@example.com", password: "password")
     @recipe_history = RecipeHistory.create!(id: 1, url: "https://example.com/recipe1", user_id: @user.id, ingredients: [])
   end
 
   test "extract & convert recipe with valid URL to desired unit" do
     post extract_recipes_url, params: { url: "https://www.allrecipes.com/mini-chicken-pot-pies-recipe-8735456", unit: "metric" }
+    assert_response :success
+  end
+
+  test "should not extract data without a URL" do
+    post extract_recipes_url, params: { unit: "metric" }
+    assert_response :redirect
+    assert_match "URL is required to extract the recipe.", flash[:alert]
+  end
+
+  test "should extract information but not convert" do
+    post extract_recipes_url, params: { url: "https://www.allrecipes.com/mini-chicken-pot-pies-recipe-8735456" }
     assert_response :success
   end
 end
