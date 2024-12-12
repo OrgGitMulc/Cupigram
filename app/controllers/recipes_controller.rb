5e0825fc35b144c6867ca8aa7e8a1a508ffb3b80
GitMulC <cmulvo@gmail.com>
2024-12-12 21:15:25 +0000

    Comments for helper method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -26,8 +26,7 @@ class RecipesController < ApplicationController
       @ingredients = ingredients
       @parsed_ingredients = parse_existing_ingredients(@ingredients)
 
-    # This code block calls the conversion helper methods found in recipes_helper  
-    
+    # This code block calls the conversion helper methods found in recipes_helper 
     if params[:unit] == "us_customary"
       @parsed_ingredients = @parsed_ingredients.map do |ingredient|
         # Will only use helpers if ingredients are in a hash
